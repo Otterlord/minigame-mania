086723a600165dcb4faae06274000e737a6f924c
Otterlord <yourotterlord@gmail.com>
2018-02-04 11:40:31 -0800

    Added unity project

---
+++ Minigame Mania/Assets/Scenes/Minigames/Catch Stuff/Scripts/CoinSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour {
+    [SerializeField] private GameObject coin;
+    public float waitTime = 3;
+    private float timeLeft;
+	
+    void Start ()
+    {
+        timeLeft = waitTime;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Timer.gameEnd) Destroy(this.gameObject);
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            timeLeft = waitTime;
+            GameObject thing = Instantiate(coin);
+            Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+            thing.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), 113.6f);
+        }
+	}
+}
---
+++ Minigame Mania/Assets/Scenes/Minigames/Catch Stuff/Scripts/PlayerBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+    public string name = "Kicky Boom";
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+
+    public GameObject scoreObject;
+    public int score = 0;
+
+    public float spd = 5;
+    private AudioSource soundPlayer;
+
+    void Start ()
+    {
+        soundPlayer = GetComponent<AudioSource>();
+       
+    }
+
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Coin") // if we collide with a coin, do stuff
+        {
+            score++;
+            scoreObject.GetComponent<TextMeshProUGUI>().text = score.ToString();
+            Destroy(other.gameObject);
+            soundPlayer.Play();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(leftKey)) transform.Translate(Vector2.left * spd * Time.deltaTime);
+        else if (Input.GetKey(rightKey)) transform.Translate(Vector2.right * spd * Time.deltaTime);
+        // Clamping
+        Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -screenBounds.x, screenBounds.x), transform.position.y);
+    }
+}
