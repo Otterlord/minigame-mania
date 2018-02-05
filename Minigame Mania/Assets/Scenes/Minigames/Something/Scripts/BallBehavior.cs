3ef92aafcf5c3286fffcfc719d592835274e08d1
Otterlord <yourotterlord@gmail.com>
2018-02-04 17:13:59 -0800

    Added a second minigame

---
+++ Minigame Mania/Assets/Scenes/Minigames/Something/Scripts/BallBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour {
+    public float force = 50;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hInput * force, 0, vInput * force);
+        movement = Vector3.ClampMagnitude(movement, force);
+
+        rb.AddForce(movement);
+	}
+}
